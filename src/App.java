0714b904787ef254ac11d5c892e17c6507d420c8
David Griswold <david.griswold@graded.br>
2023-10-18 13:29:23 -0300

    updating gitignore and some docs

--- src/App.java
+++ src/App.java
@@ -2,22 +2,32 @@ import java.util.Scanner;
 
 public class App {
     
-    private String userName;
     /**
      * This method is the primary driver method for your program.
      * It should primarily call other methods to do its work.
+     * Do NOT edit this method until after you have fully satisfied the reqs for a 5
      */
+
     public void start() {
-        userName = getUserName();
+        String userName = getUserName();
         printGreeting(userName);
         printInstructions();
         doMadlib();
+        System.out.println("hi");
     }
     
-    // TODO: create all missing methods used by the start() method
-    // You can create any other methods you need as well!
+    /* Here you should create all missing methods used by the start() method
+     * I have created the signature for ONE of the methods below.
+     * You can create any other methods you need as well!
+     */ 
     
+    public void printInstructions() {
+        
+    }
     
+  
+
+    /* DO NOT TOUCH OR EDIT THE METHOD BELOW! ADD NO CODE BELOW THIS LINE */
     public static void main(String[] args) throws Exception {
         new App().start();
     }
